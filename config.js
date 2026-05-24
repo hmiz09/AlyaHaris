06eb4e1186a526f7baed18b5bc12d75ac7698458
hmiz09 <70063656+hmiz09@users.noreply.github.com>
2026-05-24 17:57:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // Paste your Google Apps Script Web App URL between the quotes below.
 // This integrates your offline wedding planner directly with Google Sheets.
 
-const GSHEET_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzXqVCuuImVHKkvoLbu_ngHxRFyo3ml02TsTK2fTTADqDewdTKCCAMlvvmOPTOmh66v/exec";
+const GSHEET_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbySel-KVeqKoFzfB3Fp_LU_nJcc_APynBDpJqaY4TqlUSYz6s1JhztGWS5CjrNlW1h8/exec";
